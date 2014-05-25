b27c0c8f4d9d1aa220f990bd92e3f46df08af75c
RonnySanMartin <Ronnicillo@MacBook-Air-de-Ronny.local>
2014-05-25 13:01:19 -0400

    Cambio final
    Cambio en todos los códigos, ahora los datos se leen desde un archivo
    (p192, p224, p256, p383 o p521), estos archivos contienen Primo, b de
    curva, coordenadas x e y de primer punto y coordenadas x e y del
    segundo punto, en ese orden.
    se agrega código en magma para comprobar resultados y se agrega adición
    con formulas de Meloni

--- doubling.c
+++ doubling.c
@@ -4,23 +4,12 @@
 *	Autor: Ronny A. San Martin Rodriguez - LCC - USACH
 *	
 *	gcc doubling.c -o doub -lgmp
-*	./doub
+*	./doub pXXX
 *
-* 	Curva: y^2 = x^3+ax+b
-* 	P = 6277101735386680763835789423207666416083908700390324961279
-* 
-*	a = -3
-* 	b = 2455155546008943817740293915197451784769108058161191238065
+*	pXXX = Curva eliptica (192, 224, 256, 384, 521) segun estandar NIST
 *
-* 	Punto:
-* 	X1 = 602046282375688656758213480587526111916698976636884684818
-* 	Y1 = 174050332293622031404857552280219410364023488927386650641
-* 	Z1 = 1
+*	Cada archivo se compone del n primo y dos puntos (coordenas X e Y c/u)
 *
-*	2P:
-*	X2 = 5369744403678710563432458361254544170966096384586764429448
-*	Y2 = 5429234379789071039750654906915254128254326554272718558123
-*	Z2 = 1
 */
 
 #include <stdio.h>
@@ -29,10 +18,13 @@
 #include "time.h"		//Libreria para calculo del tiempo
 
 #define millon 1000000	//Divide al tiempo gastado para obtenerlo en ms
+#define maxchar 160 	//Maximo de caracteres a leer en variable cadena
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	time_586 inicio, fin;
+	FILE *archivo;			//Archivo fuente con los puntos a sumar
+	char cadena[160];		//Cadena auxiliar para pasar de archivo a mpz_t
+	time_586 inicio, fin;	//Tiempos
 	double tiempo_gastado;
 
 	mpz_t P, b;						//Curva (P primo, b de curva)
@@ -43,70 +35,92 @@ int main(void)
 
 	mpz_inits(P,b, x1,y1, x2,y2,z2, x3,y3,z3, alpha,beta, aux1,aux2,NULL);
 
-	mpz_set_str(P, "6277101735386680763835789423207666416083908700390324961279",10);
-	mpz_set_str(x1, "602046282375688656758213480587526111916698976636884684818",10);
-	mpz_set_str(y1, "174050332293622031404857552280219410364023488927386650641",10);
+	archivo = fopen(argv[1], "r");		//Lectura desde archivo fuente
+	if(archivo && argc == 2)
+	{
+		fgets(cadena, maxchar, archivo);	//Asignacion de variables
+		mpz_set_str(P, cadena, 10);
+		fgets(cadena, maxchar, archivo);
+		mpz_set_str(b, cadena, 10);
+		fgets(cadena, maxchar, archivo);
+		mpz_set_str(x1, cadena,  10);
+		fgets(cadena, maxchar, archivo);
+		mpz_set_str(y1, cadena,  10);
 
-	system("clear");
+		fclose(archivo);
 
-	time(inicio);//INICIO ALGORITMO
+		system("clear");
 
-	//Calculo de ALPHA
-	mpz_pow_ui(alpha, x1, 2);	//alpha = x1^2
-	mpz_sub_ui(alpha, alpha, 1);//alpha = x1^2 - z1^4 (z1^4 = 1)
-	mpz_mul_ui(alpha, alpha, 3);//alpha = 3*(x1^2-z1^4)
-	mpz_mod(alpha,alpha,P);		//reduccion (modulo)
+		time(inicio);//INICIO ALGORITMO
 
-	//Calculo de BETA
-	mpz_mul_ui(beta, x1, 4);	//beta = 4*x1
-	mpz_mul(beta, beta, y1);	//beta = 4*x1*y1
-	mpz_mul(beta, beta, y1);	//beta = 4*x1*y1*y1
-	mpz_mod(beta, beta, P);		//Reduccion
+		//Calculo de ALPHA
+		mpz_pow_ui(alpha, x1, 2);	//alpha = x1^2
+		mpz_sub_ui(alpha, alpha, 1);//alpha = x1^2 - z1^4 (z1^4 = 1)
+		mpz_mul_ui(alpha, alpha, 3);//alpha = 3*(x1^2-z1^4)
+		mpz_mod(alpha,alpha,P);		//reduccion (modulo)
 
-	//Calculo de Z3
-	mpz_mul_ui(z3, y1, 2);		//z3 = 2*y1*z1 (z1 = 1, trivial, no se incluye)
-	mpz_mod(z3, z3, P);			//Reduccion
+		//Calculo de BETA
+		mpz_mul_ui(beta, x1, 4);	//beta = 4*x1
+		mpz_mul(beta, beta, y1);	//beta = 4*x1*y1
+		mpz_mul(beta, beta, y1);	//beta = 4*x1*y1*y1
+		mpz_mod(beta, beta, P);		//Reduccion
 
-	//calculo de X3
-	mpz_pow_ui(x3, alpha, 2);	//x3 = alpha^2
-	mpz_submul_ui(x3, beta, 2);	//x3 = alpha^2 - 2*beta
-	mpz_mod(x3, x3, P);			//Reduccion
+		//Calculo de Z3
+		mpz_mul_ui(z3, y1, 2);		//z3 = 2*y1*z1 (z1 = 1, trivial, no se incluye)
+		mpz_mod(z3, z3, P);			//Reduccion
 
-	//calculo de Y3
-	mpz_pow_ui(aux1, y1, 4);	//aux1 = y1^4
-	mpz_sub(y3, beta, x3);		//y3   = beta-x3
-	mpz_mul(y3,alpha,y3);		//y3   = alpha*(beta-x3)
-	mpz_submul_ui(y3, aux1, 8);	//y3   = alpha*(beta-x3)-8*y1^4
-	mpz_mod(y3, y3, P);			//Reduccion
+		//calculo de X3
+		mpz_pow_ui(x3, alpha, 2);	//x3 = alpha^2
+		mpz_submul_ui(x3, beta, 2);	//x3 = alpha^2 - 2*beta
+		mpz_mod(x3, x3, P);			//Reduccion
 
-	//Transformacion jacobiano a affine
-	mpz_invert(aux1,z3,P);		//aux1 = z3^(-1) mod P
-	mpz_mul(z2,aux1,z3);		//z2   = z3*z3^(-1) = 1
-	mpz_pow_ui(aux2,aux1,3);	//aux2 = (z3^(-1))^3
-	mpz_pow_ui(aux1,aux1,2);	//aux1 = (z3^(-1))^2
-	mpz_mul(x2, x3, aux1);		//x2   = x3*(z3^(-1))^2
-	mpz_mul(y2, y3, aux2);		//y2   = y3*(z3^(-1))^3
-	
-	mpz_mod(x2, x2, P);			//Reduccion
-	mpz_mod(y2, y2, P);
-	mpz_mod(z2, z2, P);
+		//calculo de Y3
+		mpz_pow_ui(aux1, y1, 4);	//aux1 = y1^4
+		mpz_sub(y3, beta, x3);		//y3   = beta-x3
+		mpz_mul(y3,alpha,y3);		//y3   = alpha*(beta-x3)
+		mpz_submul_ui(y3, aux1, 8);	//y3   = alpha*(beta-x3)-8*y1^4
+		mpz_mod(y3, y3, P);			//Reduccion
 
-	time(fin);				//FIN ALGORITMO
-	tiempo_gastado = time_diff(fin,inicio)/millon;	//Calculo tiempo de ejecucion
-	
-	//IMPRESION DE DATOS:
-	printf("DATOS DE COORDENADAS JACOBIANAS\n\n");
-	gmp_printf("alpha = %Zd\n",alpha);
-	gmp_printf("beta  = %Zd\n",beta);
-	gmp_printf("x3    = %Zd\n", x3);
-	gmp_printf("y3    = %Zd\n", y3);
-	gmp_printf("z3    = %Zd\n", z3);
-	printf("\nRESULTADO 2P = (X,Y,Z) (affine)\n\n");
-	gmp_printf("X  = %Zd\n", x2);
-	gmp_printf("Y  = %Zd\n", y2);
-	gmp_printf("Z  = %Zd\n", z2);
+		//Transformacion jacobiano a affine
+		mpz_invert(aux1,z3,P);		//aux1 = z3^(-1) mod P
+		mpz_mul(z2,aux1,z3);		//z2   = z3*z3^(-1) = 1
+		mpz_pow_ui(aux2,aux1,3);	//aux2 = (z3^(-1))^3
+		mpz_pow_ui(aux1,aux1,2);	//aux1 = (z3^(-1))^2
+		mpz_mul(x2, x3, aux1);		//x2   = x3*(z3^(-1))^2
+		mpz_mul(y2, y3, aux2);		//y2   = y3*(z3^(-1))^3
+		
+		mpz_mod(x2, x2, P);			//Reduccion
+		mpz_mod(y2, y2, P);
+		mpz_mod(z2, z2, P);
 
-	printf("Tiempo gastado: %.20f\n\n", tiempo_gastado);
+		time(fin);				//FIN ALGORITMO
+		tiempo_gastado = time_diff(fin,inicio)/millon;	//Calculo tiempo de ejecucion
+		
+		//IMPRESION DE DATOS:
+		printf("DATOS DE CURVA\n");
+		gmp_printf("Primo = %Zd\n", P);
+		gmp_printf("b     = %Zd\n", b);
+		printf("\nPUNTOS ENTRANTES P y Q\n");
+		gmp_printf("x1    = %Zd\n", x1);
+		gmp_printf("y1    = %Zd\n", y1);
+		gmp_printf("z1    = 1\n");
+		printf("\nDATOS DE COORDENADAS JACOBIANAS\n");
+		gmp_printf("alpha = %Zd\n",alpha);
+		gmp_printf("beta  = %Zd\n",beta);
+		gmp_printf("x3    = %Zd\n", x3);
+		gmp_printf("y3    = %Zd\n", y3);
+		gmp_printf("z3    = %Zd\n", z3);
+		printf("\nRESULTADO 2P = (X,Y,Z) (affine)\n");
+		gmp_printf("X  = %Zd\n", x2);
+		gmp_printf("Y  = %Zd\n", y2);
+		gmp_printf("Z  = %Zd\n", z2);
+
+		printf("Tiempo gastado: %.20f\n\n", tiempo_gastado);
+	}
+	else
+	{
+		printf("ERROR: uso ./doub pXXX (pXXX = archivo con puntos: p192, p224, p256, p384, p521; segun curva)\n");
+	}
 
 	mpz_clears(P,b, x1,y1, x2,y2,z2, x3,y3,z3, alpha,beta, aux1,aux2, NULL);
 }
--- meloniadd.c
+++ meloniadd.c
@@ -1,10 +1,10 @@
 /*
-*	ADICION EN ECC CON APOYO DE COORDENADAS JACOBIANAS
+*	ADICION EN ECC CON APOYO DE FORMULAS DE MELONI
 *
 *	Autor: Ronny A. San Martin Rodriguez - LCC - USACH
 *	
-*	gcc addition.c -o add -lgmp
-*	./add pXXX
+*	gcc meloniadd.c -o madd -lgmp
+*	./madd pXXX
 *
 *	pXXX = Curva eliptica (192, 224, 256, 384, 521) segun estandar NIST
 *
@@ -27,13 +27,14 @@ int main(int argc, char *argv[])
 	time_586 inicio, fin;	//Tiempos
 	double tiempo_gastado;
 
-	mpz_t P, b;						//Curva (P primo, b de curva)
-	mpz_t x1, y1, x2, y2;			//Puntos Entrantes (z1=1, calculos triviales)
-	mpz_t x3, y3, z3, alpha, beta;	//Adicion en coordenadas Jacobianas
-	mpz_t x4, y4, z4;				//Punto Resultante
-	mpz_t aux1, aux2; 				//Auxiliares
+	mpz_t P, b;				//Curva (P primo, b de curva)
+	mpz_t x1, y1, x2, y2; 	//Puntos entrantes (z=1, se omite)
+	mpz_t mA, mB, mC, mD;	//Variables para F. Meloni
+	mpz_t x3, y3, z3;		//Punto resultante
+	mpz_t x4, y4, z4;
+	mpz_t aux1, aux2;		//Auxiliares
 
-	mpz_inits(P,b, x1,y1, x2,y2, x3,y3,z3, x4,y4,z4, alpha,beta, aux1,aux2,NULL);
+	mpz_inits(P,b, x1,y1, x2,y2, x3,y3,z3, x4,y4,z4, mA,mB,mC,mD, aux1,aux2, NULL);
 
 	archivo = fopen(argv[1], "r");		//Lectura desde archivo fuente
 	if(archivo && argc == 2)
@@ -50,40 +51,48 @@ int main(int argc, char *argv[])
 		mpz_set_str(x2, cadena,  10);
 		fgets(cadena, maxchar, archivo);
 		mpz_set_str(y2, cadena, 10);	
-	
+
 		fclose(archivo);
-	
+
 		system("clear");
-	
+
 		time(inicio);//INICIO ALGORITMO
-	
-		//Calculo de ALPHA
-		mpz_sub(alpha, y2, y1);		//alpha = z1^3*y2 - z2^3*y1 (z1 = z2 = 1, se omite)
-		mpz_mod(alpha, alpha, P);	//Reduccion
-	
-		//Calculo de Beta
-		mpz_sub(beta, x2, x1);		//beta = z1^2*x2 - z2^2*x1 (z1 = z2 = 1, se omite)
-		mpz_mod(beta, beta, P);		//Reduccion
-	
-		//Calculo de z3
-		mpz_set(z3, beta);		//z3 = z1*z2*beta (z1 = z2 = 1, se omite)
-								//no se reduce ya que beta se encuentra reducido
-	
-		//Calculo de x3
-		mpz_pow_ui(x3, alpha, 2);	//x3   = alpha^2
-		mpz_pow_ui(aux1, beta, 2);	//aux1 = beta^2
-		mpz_submul(x3, aux1, beta);	//x3   = alpha^2 - beta^3
-		mpz_mul(aux1, aux1,x1);		//aux1 = z2^2*x1*beta^2 (z2 = 1, se omite)
-		mpz_submul_ui(x3, aux1, 2);	//x3   = alpha^2 - beta^3 -2*z2^2*x1*beta^2 (z2 = 1, se omite)
-		mpz_mod(x3, x3, P);			//Reduccion
-	
-		//calculo de y3
-		mpz_sub(y3, aux1, x3);		//y3   = z2^2*x1*beta^2 - x3
-		mpz_mul(y3, alpha, y3);		//y3   = aplha*(z2^2*x1*beta^2 - x3)
-		mpz_pow_ui(aux1, beta, 3);	//aux1 = z2^3*beta^3
-		mpz_submul(y3, aux1, y1);	//y3   = aplha*(z2^2*x1*beta^2 - x3) - z2^2*y1*beta^3
-		mpz_mod(y3, y3, P);			//Reduccion
-	
+
+		//Calculo de mA
+		mpz_sub(mA, x2, x1);
+		mpz_pow_ui(mA, mA, 2);
+		mpz_mod(mA, mA, P);		//Reduccion
+
+		//Calculo de mB
+		mpz_mul(mB, x1, mA);
+		mpz_mod(mB, mB, P);		//Reduccion
+
+		//Calculo de mC
+		mpz_mul(mC, x2, mA);
+		mpz_mod(mC, mC, P);		//Reduccion
+
+		//Calculo de mD
+		mpz_sub(mD, y2, y1);
+		mpz_pow_ui(mD, mD, 2);
+		mpz_mod(mD, mD, P);		//Reduccion
+
+		//Calculo de X3
+		mpz_sub(x3, mD, mB);
+		mpz_sub(x3, x3, mC);
+		mpz_mod(x3, x3, P);		//Reduccion
+
+		//Calculo de Y3
+		mpz_sub(aux1, y2, y1);
+		mpz_sub(y3, mB, x3);
+		mpz_mul(y3, y3, aux1);
+		mpz_sub(aux1, mC, mB);
+		mpz_submul(y3, aux1, y1);
+		mpz_mod(y3, y3, P);		//Reduccion
+
+		//Calculo de Z3
+		mpz_sub(z3, x2, x1);
+		mpz_mod(z3, z3, P);		//Reduccion
+
 		//Transformacion jacobiano a affine
 		mpz_invert(aux1,z3,P);		//aux1 = z3^(-1) mod P
 		mpz_mul(z4,aux1,z3);		//z2   = z3*z3^(-1) = 1
@@ -95,10 +104,10 @@ int main(int argc, char *argv[])
 		mpz_mod(x4, x4, P);			//Reduccion
 		mpz_mod(y4, y4, P);
 		mpz_mod(z4, z4, P);
-	
+
+
 		time(fin);				//FIN ALGORITMO
 		tiempo_gastado = time_diff(fin,inicio)/millon;	//Calculo tiempo de ejecucion
-	
 		//IMPRESION DE DATOS:
 		printf("DATOS DE CURVA\n");
 		gmp_printf("Primo = %Zd\n", P);
@@ -110,23 +119,22 @@ int main(int argc, char *argv[])
 		gmp_printf("x2    = %Zd\n", x2);
 		gmp_printf("y2    = %Zd\n", y2);
 		gmp_printf("z2    = 1\n");
-		printf("\nDATOS DE COORDENADAS JACOBIANAS\n");
-		gmp_printf("alpha = %Zd\n", alpha);
-		gmp_printf("beta  = %Zd\n", beta);
-		gmp_printf("x3    = %Zd\n", x3);
-		gmp_printf("y3    = %Zd\n", y3);
-		gmp_printf("z3    = %Zd\n", z3);
-		printf("\nRESULTADO P+Q = (X,Y,Z)\n");
-		gmp_printf("X     = %Zd\n", x4);
-		gmp_printf("Y     = %Zd\n", y4);
-		gmp_printf("Z     = %Zd\n", z4);
-	
+		printf("\nDATOS DE FORMULA DE MELONI\n");
+		gmp_printf("A     = %Zd\n", mA);
+		gmp_printf("B     = %Zd\n", mB);
+		gmp_printf("C     = %Zd\n", mC);
+		gmp_printf("D     = %Zd\n", mD);
+		printf("\nRESULTADO P+Q = (X',Y',Z')\n");
+		gmp_printf("X'    = %Zd\n", x4);
+		gmp_printf("Y'    = %Zd\n", y4);
+		gmp_printf("Z'    = %Zd\n", z4);
 		printf("\nTiempo gastado: %.20f\n\n", tiempo_gastado);
 	}
 	else
 	{
-		printf("ERROR: uso ./add pXXX (pXXX = archivo con puntos: p192, p224, p256, p384, p521; segun curva)\n");
+		printf("ERROR: uso ./madd pXXX (pXXX = archivo con puntos: p192, p224, p256, p384, p521; segun curva)\n");
 	}
 
-	mpz_clears(P,b, x1,y1, x2,y2, x3,y3,z3, x4,y4,z4, alpha,beta, aux1,aux2, NULL);
+	mpz_clears(P,b, x1,y1, x2,y2, x3,y3,z3, x4,y4,z4, mA,mB,mC,mD, aux1,aux2, NULL);
+
 }
